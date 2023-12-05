5ba5cad09f03017efb6a463fce00dbdfd3f67626
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-12-05 08:19:36 -0800

    Python DSA

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 
 def new_in_list(my_list, idx, element):
     if (idx >= len(my_list)) or (idx < 0):
-        lcopy = my_list
+        lcopy = my_list.copy()
         return lcopy
-    else:
+    elif (idx >= 0) and (idx < len(my_list)):
         new_list = my_list.copy()
         new_list[idx] = element
         return(new_list)
