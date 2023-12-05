c3cb6fd6ab0fd0c66962e2ed7566d951761659c4
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-12-04 17:17:43 -0800

    python_DSA

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,4 +4,4 @@ def replace_in_list(my_list, idx, element):
     if (idx < 0) or (idx >= len(my_list)):
         return my_list
     elif (idx > 0) and (idx < len(my_list)):
-        return(my_list[idx]=element)
+        my_list[idx] = element
