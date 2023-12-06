427bb02de55defb2337a4402321fd7dae8e60315
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-12-06 11:08:24 -0800

    Python modules & import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    
+if __name__ == "__main__":    
     import sys
     arg_number = len(sys.argv) - 1
-    
     if arg_number == 0:
         print("0 argument.")
     else:
         print("{} arguments:".format(arg_number))
-    
     for i, arguments in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(i, arguments))
