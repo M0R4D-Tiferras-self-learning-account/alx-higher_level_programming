803f879a92467fb6bc98ed5bbc199b7a2e19e5cd
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-12-08 13:35:32 -0800

    Python_DSA

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    if search in new_list:
-        new_list[search] = replace
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
     return new_list
