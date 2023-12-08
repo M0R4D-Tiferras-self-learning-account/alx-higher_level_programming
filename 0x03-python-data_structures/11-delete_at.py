be785de7c9ae2a4bdd52dcec1a2d414d63ce313a
M0R4D-Tiferras-self-learning-account <m.tiferras8@gmail.com>
2023-12-08 11:58:35 -0800

    python_DSA

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    n_idx = -1 * (len(my_list) - 1)
-    if (idx >= len(my_list)) or (idx < n_idx):
+    n_idx = -1 * (len(my_list))
+    if (idx >= len(my_list)) or (idx < n_indx):
         return my_list
     else:
         new_list = my_list.copy()
