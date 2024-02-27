641d5ce53c562db51cf6870c373158b07e386a88
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-02-27 15:15:00 -0800

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
+    
+    
+    if len(sys.argv) == 4:
+        if sys.argv[2] == "+":
+           print(add(int(sys.argv[1]), int(sys.argv[3])))
+        elif sys.argv[2] == "-":
+            print(sub(int(sys.argv[1]), int(sys.argv[3])))
+        elif sys.argv[2] == "*":
+            print(mul(int(sys.argv[1]), int(sys.argv[3])))
+        elif sys.argv[2] == "/":
+            print(div(int(sys.argv[1]), int(sys.argv[3])))
+        else:
+            print("Unknown operator. Available operators: +, -, * and / ")
+            sys.exit("1")
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit("1")
+
