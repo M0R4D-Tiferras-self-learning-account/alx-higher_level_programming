69cbd46f3306bd1aeb1b97de8497b8670d23fe0b
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-04-06 23:53:47 -0700

    OOP

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+"""Define a class Square"""
+
+
 class Square:
+    """
+    A class that represent a square
+
+    Attributes:
+        size (int): The size of the square.
+    """
     def __init__(self, size):
         self.__size = size
