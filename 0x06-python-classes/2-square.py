be20c1acb7c5c39fa5472fb362a96e1907fe93da
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-04-07 08:03:35 -0700

    OOP

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 
+"""Initiallizing Square class"""
+
+
 class Square:
+    """
+    A class that represent a square
+
+    Attributes:
+        size (int): The size of the square.
+    """
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+        if type(size) == int:
+            if size > 0:
+                self.size = size
+                return
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+                return
+        else:
+            raise TypeError("size must be an integer")
