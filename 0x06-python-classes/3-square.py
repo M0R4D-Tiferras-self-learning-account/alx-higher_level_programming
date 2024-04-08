e0e39f2059b810aad524cdb3597ac8d836894722
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-04-08 06:10:19 -0700

    OOP

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,26 @@
-#!/usr/bin/python3
+#/bin/usr/python3
+
+"""Defining a class called square"""
+
 
 class Square:
+    """
+    A class that represent a square
+
+    Attributes:
+        size (int): The size of the square.
+    """
     def __init__(self, size=0):
-        if (type(size) is not int):
-            raise (TypeError("size must be an integer"))
-        elif (size < 0):
-            raise (ValueError("size must be >= 0"))
-        else:
-            self.__size = size
+        try:
+            if size >= 0 and type(size) == int:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        except TypeError:
+            raise TypeError("size must be an integer")
+    
     def area(self):
-        return (self.__size ** 2)
+        """A function that return the area of the square
+        """
+        area = self.__size * self.__size
+        return area
