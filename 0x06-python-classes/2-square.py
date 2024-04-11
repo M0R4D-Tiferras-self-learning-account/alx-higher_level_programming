f5effd29e6bbb683b7c70b4e0740587516702db3
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-04-11 03:19:07 -0700

    OOP

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -22,7 +22,7 @@ class Square:
         """
         if isinstance(size, int):
             if size > 0:
-                self.__size = size
+                self._size = size
             elif size < 0:
                 raise ValueError("size must be >= 0")
         else:
