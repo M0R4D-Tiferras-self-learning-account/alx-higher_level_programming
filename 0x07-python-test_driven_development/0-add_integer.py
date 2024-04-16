fcef5e4e0cd73ebe7e12895de3ea564956ef966c
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-04-16 09:59:09 -0700

    Unit_Testing

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,11 @@
 #!/bin/bash/python3
 
+""" a func that add tow number """
 
+def add_integer(a, b=98):
+    """add_integer: a function that add tow num
+
+    Args:
+    a (integer, float): first num to add
+    b (integer, float): second num to add
+    """
