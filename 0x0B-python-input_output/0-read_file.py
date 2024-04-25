fb824ae390a2370eb8fa34153ad9b3fd871112e2
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-04-25 16:11:27 +0000

    python I/O

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,11 +2,12 @@
 
 """ A func that rad files"""
 
-def  read_file(filename=""):
+
+def read_file(filename=""):
     """a function that reads a file:
 
     Args:
-        filename (str, optional): the file name.
+        filename (str): the file name.
     """
     with open(filename, 'r', encoding="utf8") as file:
         content = file.read()
