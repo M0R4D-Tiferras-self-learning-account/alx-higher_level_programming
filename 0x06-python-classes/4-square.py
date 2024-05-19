affc2e9db3612c759f11a2b6f2cab385d4e3d085
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-05-19 04:15:57 -0700

    OOP

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,46 +1,44 @@
-#!/bin/usr/python3
-
-"""Area of a square"""
+#!/usr/bin/python3
+""" class Square that defines a square"""
 
 
 class Square:
-    """
-    A class that represent a square
-
-    Attributes:
-        size (int): The size of the square.
-        area (int): The area of the square.
-    """
+    """ class Square that defines a square"""
     def __init__(self, size=0):
-        """Creates new instances of the square
+        """ init square
 
         Args:
-            size (int): size of the square
+            value (int): size of the square.
         """
-
-        if type(size) != int:
-            raise TypeError("size must be an integer ")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        """A function that return the area of the square
-
-        Returns:
-            area (int): The area of the square.
-        """
-        area = self.__size ** 2
-        return area
+        self.size = size
 
     @property
     def size(self):
+        """int: private size.
+
+        Returns:
+            Private size.
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError("size must be an integer ")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+        """Sets value into size, must be int.
+
+        Args:
+            value (int): size of the square.
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value  #: size of the square
+
+    def area(self):
+        """returns the area
+
+        Returns:
+            area.
+        """
+        return self.__size**2
