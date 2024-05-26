aecfb9de28b06a51ab0dd96536ef490690c10e65
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-05-26 11:08:44 +0000

    MySQL

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates the user user_0d_1 with all privileges on the MySql server
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
